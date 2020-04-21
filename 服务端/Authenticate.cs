466efd123a4863b27d9d0f1bbeb5f762bbb7be0b
宋润涵 <53166321+sh963852741@users.noreply.github.com>
2020-04-21 20:31:19 +0800

    程序框架

---
+++ 服务端/Authenticate.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace 服务端
+{
+    /// <summary>
+    /// 验证用户身份
+    /// </summary>
+    class Authenticate
+    {
+        /// <summary>
+        /// 服务器端的秘钥
+        /// </summary>
+        private string secret;
+
+        /// <summary>
+        /// 用户注册
+        /// </summary>
+        /// <param name="userName">用户名</param>
+        /// <param name="password">密码</param>
+        /// <param name="type">许可证类型</param>
+        /// <returns>序列号</returns>
+        public string Registe(string userName,string password,string type)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// 用户登录
+        /// </summary>
+        /// <param name="serialNumber">序列号</param>
+        /// <returns>令牌</returns>
+        public string LogIn(string serialNumber)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ 服务端/Authorize.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace 服务端
+{
+    struct UserInfo
+    {
+        string userName;
+        Guid userID;
+    }
+
+    /// <summary>
+    /// 对客户端的请求进行授权
+    /// </summary>
+    public class Authorize
+    {
+        /// <summary>
+        /// 服务器端的秘钥
+        /// </summary>
+        private string secret;
+
+        /// <summary>
+        /// 给予验证身份后的用户使用授权
+        /// </summary>
+        /// <param name="token">令牌</param>
+        /// <returns>是否取得授权</returns>
+        public bool GetAuthorization(string token)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// 验证令牌是否有效
+        /// </summary>
+        /// <param name="token">令牌</param>
+        /// <returns>是否有效</returns>
+        private bool VerifyToken(string token)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// 获取令牌中的信息
+        /// </summary>
+        /// <param name="token">有效的令牌</param>
+        /// <returns>用户信息</returns>
+        private UserInfo DecodeToken(string token)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// 检查授权数是否达到上限
+        /// </summary>
+        /// <param name="userInfo">用户信息</param>
+        /// <returns>是否授权成功</returns>
+        private bool TryAuthorize(UserInfo userInfo)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// 生成新令牌
+        /// </summary>
+        /// <param name="part1"></param>
+        /// <param name="part2"></param>
+        /// <param name="secret"></param>
+        /// <returns></returns>
+        public string MakeNewToken(string part1,string part2,string secret)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
