d07bc1b04ee45a79d237f37ec73526dc07427449
quxc551 <60848379+quxc551@users.noreply.github.com>
2020-05-12 14:44:02 +0800

    update

--- WebApplication1/WebApplication1/Authenticate.cs
+++ WebApplication1/WebApplication1/Authenticate.cs
@@ -11,6 +11,26 @@ using WebApplication1;
 
 namespace WebApplication1
 {
+
+    public class ClientMessage
+    {
+        /// <summary>
+        /// 客户端地址信息
+        /// </summary>
+        /// 
+        public IPEndPoint clientIPEndPoint;
+        /// <summary>
+        /// 客户端发来的序列号
+        /// </summary>
+        public string serialNum;
+
+        public ClientMessage(IPEndPoint point, string mess)
+        {
+            clientIPEndPoint = new IPEndPoint(point.Address, point.Port);
+            serialNum = mess;
+        }
+    }
+
     public struct UserData
     {
         public string serialNumber;
@@ -34,20 +54,28 @@ namespace WebApplication1
         public delegate void OnDataRecv(byte[] data);
         private UdpClient client = new UdpClient(8910, AddressFamily.InterNetwork);
         private string secret = "75012";
-        private List<UserData> users=new List<UserData>();
+        //private List<UserData> users=new List<UserData>();
+        private RegistedUser registedUser = new RegistedUser();
 
         public Authenticate()
         {
-            FileStream fs = new FileStream("RUser.db", FileMode.OpenOrCreate, FileAccess.Read, FileShare.ReadWrite);
-            StreamReader reader= new StreamReader(fs);
-            while(!reader.EndOfStream)
-            {
-                string regRecord = reader.ReadLine();
-                users.Add(new UserData(regRecord.Split('\0')));
-            }
-            OnDataRecv callback = new OnDataRecv(ProcessData);
+            //string a = Registe("wang", "1", "1");
+           // FileStream fs = new FileStream("RUser.db", FileMode.OpenOrCreate, FileAccess.Read, FileShare.ReadWrite);
+            //StreamReader reader= new StreamReader(fs);
+            registedUser.ReadFromFile();
+            registedUser.WriteToFile();
+
+            //registedUser.ReadFromFile();
+            //while(!reader.EndOfStream)
+            //{
+            //    string regRecord = reader.ReadLine();
+            //    users.Add(new UserData(regRecord.Split('\0')));
+            //}
+
+
+            // OnDataRecv callback = new OnDataRecv(ProcessData);
             Thread th = new Thread(UDPListener);
-            th.Start(callback);
+            th.Start();
         }
 
         /// <summary>
@@ -62,12 +90,18 @@ namespace WebApplication1
             string serialNumber = string.Empty;
             for (int i = 0; i < 10; ++i)
                 serialNumber += new Random().Next(1, 9).ToString();
-            string[] regRecord = { serialNumber, userName, password, type };
-            users.Add(new UserData(regRecord));
-            FileStream fs = new FileStream("RUser.db", FileMode.Append, FileAccess.Write, FileShare.ReadWrite);
-            StreamWriter writer = new StreamWriter(fs);
-            writer.WriteLine(serialNumber + '\0' + userName + '\0' + password + '\0' + type);
-            writer.Close();
+
+            RegRecord regRecord1 = new RegRecord(userName, password, serialNumber, int.Parse(type));
+            registedUser.ReadFromFile();
+            registedUser.Add(regRecord1);
+            registedUser.WriteToFile();
+
+            //string[] regRecord = { serialNumber, userName, password, type };
+            //users.Add(new UserData(regRecord));
+            //FileStream fs = new FileStream("RUser.db", FileMode.Append, FileAccess.Write, FileShare.ReadWrite);
+            //StreamWriter writer = new StreamWriter(fs);
+            //writer.WriteLine(serialNumber + '\0' + userName + '\0' + password + '\0' + type);
+            //writer.Close();
             return serialNumber;
         }
 
@@ -78,14 +112,9 @@ namespace WebApplication1
         /// <returns>令牌</returns>
         public string LogIn(string serialNumber)
         {
-            int pos = users.FindIndex(e => e.serialNumber == serialNumber);
-            if (pos == -1)
+            registedUser.ReadFromFile();
+            if (registedUser.Contains(serialNumber))
             {
-                return null;
-            }
-
-            else
-            {               
                 var header = new
                 {
                     typ = "JWT",
@@ -93,95 +122,126 @@ namespace WebApplication1
                 };
                 var payload = new
                 {
-                    iat = DateTime.Now,
-                    exp = 1800,
-                    userName = users[pos].userName,
+                    iat = DateTime.Now.AddHours(10),
+                    userName = registedUser.GetUser(serialNumber).userName,
                     id = Guid.NewGuid()
                 };
-
-                //将每个用户下的guid号记录到AuthorizationData.txt中
-                FileStream fs = new FileStream("RUser.db", FileMode.OpenOrCreate, FileAccess.Read, FileShare.ReadWrite);
-                StreamReader reader = new StreamReader(fs);
-                StreamWriter writer = new StreamWriter("D:\\AuthorizationData.txt");
-                int FilLines = 0;
-                int line = 0;
-                while (!reader.EndOfStream)
-                {
-                    string AutRecord = reader.ReadLine();
-                    ++FilLines;
-                    if (AutRecord.Substring(0, AutRecord.IndexOf('\0') - 1).Equals(payload.id))
-                        line= FilLines;
-                }
-                if (line==0)
-                {
-                    writer.WriteLine(payload.userName + '\0' + payload.id);
-                }
-                else
-                {
-                    string[] str = File.ReadAllLines("D:\\AuthorizationData.txt");
-                    string[] str2 = str[line].Split('\0');
-                    int i = 1;
-                    for (;i<str2.Length;++i)
-                    {
-                        if (str2[i].Equals(payload.id)) break;
-                    }
-                    if (i == str2.Length)
-                    {
-                        str[line] += '\0' + payload.id.ToString();
-                    }
-                    File.WriteAllText("D:\\AuthorizationData.txt", string.Empty);
-                    for(int j = 0; j < str.Length; ++j)
-                    {
-                        writer.WriteLine(str[j]);
-                    }
-                }
-                reader.Close();
-                writer.Close();
-
                 string part1 = Convert.ToBase64String(Encoding.Default.GetBytes(JsonConvert.SerializeObject(header)));
                 string part2 = Convert.ToBase64String(Encoding.Default.GetBytes(JsonConvert.SerializeObject(payload)));
-                string res = part1 + part2 + secret;
-                HMACSHA256 maker = new HMACSHA256();
+                string res = part1 + part2 ;
+                HMACSHA256 maker = new HMACSHA256(System.Text.Encoding.Default.GetBytes(secret));
                 string signature = Encoding.Default.GetString(maker.ComputeHash(Encoding.Default.GetBytes(res)));
-                return part1 + part2 + signature;
+                string result = $"{part1}.{part2}.{signature}";
+                return result;
             }
+            else
+                return null;
+
+            //int pos = users.FindIndex(e => e.serialNumber == serialNumber);
+            //if (pos == -1)
+            //{
+            //    return null;
+            //}
+
+            //else
+            //{               
+            //    var header = new
+            //    {
+            //        typ = "JWT",
+            //        alg = "HS256"
+            //    };
+            //    var payload = new
+            //    {
+            //        iat = DateTime.Now,
+            //        exp = 1800,
+            //        userName = users[pos].userName,
+            //        id = Guid.NewGuid()
+            //    };
+
+            //    //将每个用户下的guid号记录到AuthorizationData.txt中
+            //    FileStream fs = new FileStream("RUser.db", FileMode.OpenOrCreate, FileAccess.Read, FileShare.ReadWrite);
+            //    StreamReader reader = new StreamReader(fs);
+            //    StreamWriter writer = new StreamWriter("D:\\AuthorizationData.txt");
+            //    int FilLines = 0;
+            //    int line = 0;
+            //    while (!reader.EndOfStream)
+            //    {
+            //        string AutRecord = reader.ReadLine();
+            //        ++FilLines;
+            //        if (AutRecord.Substring(0, AutRecord.IndexOf('\0') - 1).Equals(payload.id))
+            //            line= FilLines;
+            //    }
+            //    if (line==0)
+            //    {
+            //        writer.WriteLine(payload.userName + '\0' + payload.id);
+            //    }
+            //    else
+            //    {
+            //        string[] str = File.ReadAllLines("D:\\AuthorizationData.txt");
+            //        string[] str2 = str[line].Split('\0');
+            //        int i = 1;
+            //        for (;i<str2.Length;++i)
+            //        {
+            //            if (str2[i].Equals(payload.id)) break;
+            //        }
+            //        if (i == str2.Length)
+            //        {
+            //            str[line] += '\0' + payload.id.ToString();
+            //        }
+            //        File.WriteAllText("D:\\AuthorizationData.txt", string.Empty);
+            //        for(int j = 0; j < str.Length; ++j)
+            //        {
+            //            writer.WriteLine(str[j]);
+            //        }
+            //    }
+            //    reader.Close();
+            //    writer.Close();
+
+            //    string part1 = Convert.ToBase64String(Encoding.Default.GetBytes(JsonConvert.SerializeObject(header)));
+            //    string part2 = Convert.ToBase64String(Encoding.Default.GetBytes(JsonConvert.SerializeObject(payload)));
+            //    string res = part1 + part2 + secret;
+            //    HMACSHA256 maker = new HMACSHA256();
+            //    string signature = Encoding.Default.GetString(maker.ComputeHash(Encoding.Default.GetBytes(res)));
+            //    return part1 + part2 + signature;
+            //}
         }
 
         public void UDPListener(object obj)
         {
             byte[] recvBuffer = new byte[1024];
-            IPEndPoint ip = new IPEndPoint(IPAddress.Parse("192.168.18.3"), 8910);
-            recvBuffer=client.Receive(ref ip);
-            OnDataRecv CallBack = obj as OnDataRecv;
-            CallBack(recvBuffer);
-            string token = LogIn(getSerialNumber()); //用户登录，获得令牌；
-            sentToken(token); //将令牌发送给客户端
+
+            while (true)//持续监听端口
+            {
+
+                ClientMessage message = getSerialNumber();
+                ProcessData(message);
+            }
         }
 
-        public void ProcessData(byte[] data)
+        public void ProcessData(ClientMessage clientMessage)
         {
-            IPEndPoint ip = new IPEndPoint(IPAddress.Parse("192.168.18.3"), 6000);
-            string userinfo = System.Text.Encoding.UTF8.GetString(data);
-            string[] arr = userinfo.Split('\0');
-            byte[] sendbuffer = Encoding.Default.GetBytes(Registe(arr[0], arr[1], arr[2]));
-            client.Send(sendbuffer, sendbuffer.Length, ip);
+            string serialNum = clientMessage.serialNum;
+            string token = LogIn(serialNum);
+            sentToken(token, clientMessage.clientIPEndPoint);
         }
 
         //获得用户注册后的序列号
-        public string getSerialNumber()
+        public ClientMessage getSerialNumber()
         {
-            IPEndPoint ip = new IPEndPoint(IPAddress.Parse("192.168.18.3"), 6000);
+            IPEndPoint ip = new IPEndPoint(IPAddress.Any, 0);
             byte[] recvBuffer = new byte[11];
             recvBuffer = client.Receive(ref ip);
-            return Encoding.UTF8.GetString(recvBuffer);
+            ClientMessage clientMessage = new ClientMessage(ip, Encoding.UTF8.GetString(recvBuffer));
+            return clientMessage;
         }
-        
+
         //将令牌发送到客户端
-        public void sentToken(string token)
+        public void sentToken(string token, IPEndPoint iP)
         {
-            IPEndPoint ip = new IPEndPoint(IPAddress.Parse("192.168.18.3"), 6000);
+            if (token == null)
+                token = "";
             byte[] sendbuffer = Encoding.Default.GetBytes(token);
-            client.Send(sendbuffer, sendbuffer.Length,ip);
+            client.Send(sendbuffer, sendbuffer.Length, iP);
         }
     }
 }
